0e690b5686d223aaaa43f33602c752c9601dd977
indigoHeart <rafalbudny7@gmail.com>
2020-01-29 20:30:52 +0100

    init trackviewmodel
    changes in .gitignore

--- someForms/ViewModels/TrackViewModel.cs
+++ someForms/ViewModels/TrackViewModel.cs
@@ -7,5 +7,26 @@ namespace someForms.ViewModels
     [POCOViewModel]
     public class TrackViewModel
     {
+        private TrackInfo _track;
+        public virtual int TrackId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Composer { get; set; }
+        private void Load(TrackInfo track)
+        {
+            this._track = track;
+            this.TrackId = _track.TrackId;
+            this.Name = _track.Name;
+            this.Composer = _track.Composer;
+        }
+
+        private void Store()
+        {
+            if (TrackId != _track.TrackId)
+                _track.TrackId = TrackId;
+            if (!string.Equals(Name, _track.Name))
+                _track.Name = Name;
+            if (!string.Equals(Composer, _track.Composer))
+                _track.Composer = Composer;
+        }
     }
 }
