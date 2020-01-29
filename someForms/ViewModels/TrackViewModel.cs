30100a8b67fac5ca19f1bdbab7db440ff481393c
indigoHeart <rafalbudny7@gmail.com>
2020-01-29 19:57:07 +0100

    Add track mv and v

---
+++ someForms/ViewModels/TrackViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm;
+
+namespace someForms.ViewModels
+{
+    [POCOViewModel]
+    public class TrackViewModel
+    {
+    }
+}
